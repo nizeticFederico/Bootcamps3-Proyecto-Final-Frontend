663bbf40c31a50a15a3c83f7d75f723c3334d43f
ElRubenD <rubem.d.ledesma@gmail.com>
2024-10-27 22:27:49 -0300

    vista About y boton de scroll para arriba

---
+++ quicktickets/src/app/(site)/about/index.tsx
@@ -0,0 +1,211 @@
+import Image from "next/image"
+
+export default async function AboutUs(){
+    return(
+        <main className="flex flex-col items-center min-h-screen">
+            <div className="w-full h-52 bg-[url('../../public/assets/images/about.jpg')] bg-cover bg-center flex items-center justify-center">
+                <h2 className="font-bold text-4xl text-white text-center">About Us</h2>
+            </div>
+            <div className="flex-grow">
+                <h3 className="font-bold text-2xl text-[#2B293D] text-start p-5 leading-[1.5]">Nuestra Historia</h3>
+                <p className=" text-[#2B293D] p-8 leading-[1.5]">Todo comenzó en 2024, cuando, por causa del destino, logramos ser seleccionados para el Bootcamp 3.0 organizado por la empresa Devlights. En ese espacio, tuvimos la oportunidad de explorar nuevas herramientas y profundizar en lenguajes que ya habíamos estudiado. El objetivo del Bootcamp era desarrollar una página web donde pudiéramos volcar todo lo aprendido y, si era posible, un poco más. <br /> <br />Así nació QuickTickets, nuestra plataforma para eventos. Al principio, barajamos diversas alternativas, y fue entonces cuando un compañero encontró la opción que mejor se adaptaba a nuestras necesidades. Con entusiasmo, nos repartimos las tareas y organizamos las actividades, construyendo la página poco a poco. La colaboración y el diálogo fueron fundamentales; utilizamos tableros de Trello para gestionar nuestros proyectos, creamos grupos en Discord para compartir ideas y manteníamos una comunicación constante a través de WhatsApp. <br /> <br />La experiencia del Bootcamp fue más que un simple aprendizaje técnico; nos enseñó sobre el poder del trabajo en equipo y la importancia de la empatía. A medida que QuickTickets tomaba forma, nos dimos cuenta de que estábamos creando algo significativo, una herramienta que podría transformar la forma en que las personas se conectan con eventos en nuestra comunidad. <br /> <br />Con el tiempo, no solo nos enfocamos en el desarrollo técnico, sino que también aprendimos a valorar las relaciones construidas durante el proceso. Cada reunión, cada conversación, y cada idea compartida nos acercaron más a nuestro objetivo común, y al final, QuickTickets se convirtió en una representación del esfuerzo colectivo y la pasión que todos pusimos en este proyecto.</p>
+                <h3 className="font-bold text-2xl text-[#2B293D] text-start p-5 leading-[1.5]">Fundadores</h3>
+            </div>
+            <div className="flex flex-wrap justify-center w-full p-8 space-x-4 ">
+                <div className="w-56 h-60 bg-white p-5 rounded-lg cursor-pointer transition duration-500 hover:shadow-lg hover:shadow-gray-500">
+                        <div className="h-1.5 bg-gray-300 rounded-full mb-4"></div>
+                        <div className="flex flex-col items-center">
+                            <Image
+                                src="/assets/images/user-default.png"
+                                width={70}
+                                height={70}
+                                alt="Picture of the author"
+                            />
+                        <strong className="text-lg text-gray-800">Fede Nizetic</strong>
+                        </div>
+                        <div className="flex justify-center mt-4 space-x-4 text-gray-600">
+                        <a href="" target="_blank">
+                                <Image
+                                    src="/assets/images/linkedin.png"
+                                    width={30}
+                                    height={30}
+                                    alt="Picture of the author"
+                                />
+                            </a>
+                            <a href="" target="_blank" >
+                                <Image
+                                    src="/assets/images/github.png"
+                                    width={30}
+                                    height={30}
+                                    alt="Picture of the author"
+                                />
+                            </a>
+                            <a href="" target="_blank" >
+                                <Image
+                                    src="/assets/images/sobre.png"
+                                    width={30}
+                                    height={30}
+                                    alt="Picture of the author"
+                                />
+                            </a>
+                        </div>
+                </div>
+                <div className="w-56 h-60 bg-white p-5 rounded-lg cursor-pointer transition duration-500 hover:shadow-lg hover:shadow-gray-500">
+                        <div className="h-1.5 bg-gray-300 rounded-full mb-4"></div>
+                        <div className="flex flex-col items-center">
+                            <Image
+                                src="/assets/images/user-default.png"
+                                width={70}
+                                height={70}
+                                alt="Picture of the author"
+                            />
+                        <strong className="text-lg text-gray-800">Augusto Catterino</strong>
+                        </div>
+                        <div className="flex justify-center mt-4 space-x-4 text-gray-600">
+                        <a href="" target="_blank">
+                                <Image
+                                    src="/assets/images/linkedin.png"
+                                    width={30}
+                                    height={30}
+                                    alt="Picture of the author"
+                                />
+                            </a>
+                            <a href="" target="_blank" >
+                                <Image
+                                    src="/assets/images/github.png"
+                                    width={30}
+                                    height={30}
+                                    alt="Picture of the author"
+                                />
+                            </a>
+                            <a href="" target="_blank" >
+                                <Image
+                                    src="/assets/images/sobre.png"
+                                    width={30}
+                                    height={30}
+                                    alt="Picture of the author"
+                                />
+                            </a>
+                        </div>
+                </div>
+
+                <div className="w-56 h-60 bg-white p-5 rounded-lg cursor-pointer transition duration-500 hover:shadow-lg hover:shadow-gray-500">
+                        <div className="h-1.5 bg-gray-300 rounded-full mb-4"></div>
+                        <div className="flex flex-col items-center">
+                            <Image
+                                src="/assets/images/user-default.png"
+                                width={70}
+                                height={70}
+                                alt="Picture of the author"
+                            />
+                        <strong className="text-lg text-gray-800">Pablo Merino</strong>
+                        </div>
+                        <div className="flex justify-center mt-4 space-x-4 text-gray-600">
+                        <a href="" target="_blank">
+                                <Image
+                                    src="/assets/images/linkedin.png"
+                                    width={30}
+                                    height={30}
+                                    alt="Picture of the author"
+                                />
+                            </a>
+                            <a href="" target="_blank" >
+                                <Image
+                                    src="/assets/images/github.png"
+                                    width={30}
+                                    height={30}
+                                    alt="Picture of the author"
+                                />
+                            </a>
+                            <a href="" target="_blank" >
+                                <Image
+                                    src="/assets/images/sobre.png"
+                                    width={30}
+                                    height={30}
+                                    alt="Picture of the author"
+                                />
+                            </a>
+                        </div>
+                </div>
+
+                <div className="w-56 h-60 bg-white p-5 rounded-lg cursor-pointer transition duration-500 hover:shadow-lg hover:shadow-gray-500">
+                        <div className="h-1.5 bg-gray-300 rounded-full mb-4"></div>
+                        <div className="flex flex-col items-center">
+                            <Image
+                                src="/assets/images/user-default.png"
+                                width={70}
+                                height={70}
+                                alt="Picture of the author"
+                            />
+                        <strong className="text-lg text-gray-800">Rodolfo Villalba</strong>
+                        </div>
+                        <div className="flex justify-center mt-4 space-x-4 text-gray-600">
+                        <a href="" target="_blank">
+                                <Image
+                                    src="/assets/images/linkedin.png"
+                                    width={30}
+                                    height={30}
+                                    alt="Picture of the author"
+                                />
+                            </a>
+                            <a href="" target="_blank" >
+                                <Image
+                                    src="/assets/images/github.png"
+                                    width={30}
+                                    height={30}
+                                    alt="Picture of the author"
+                                />
+                            </a>
+                            <a href="" target="_blank" >
+                                <Image
+                                    src="/assets/images/sobre.png"
+                                    width={30}
+                                    height={30}
+                                    alt="Picture of the author"
+                                />
+                            </a>
+                        </div>
+                </div>
+
+                <div className="w-56 h-60 bg-white p-5 rounded-lg cursor-pointer transition duration-500 hover:shadow-lg hover:shadow-gray-500">
+                        <div className="h-1.5 bg-gray-300 rounded-full mb-4"></div>
+                        <div className="flex flex-col items-center">
+                            <Image
+                                src="/assets/images/user-default.png"
+                                width={70}
+                                height={70}
+                                alt="Picture of the author"
+                            />
+                        <strong className="text-lg text-gray-800">Ruben Ledesma</strong>
+                        </div>
+                        <div className="flex justify-center mt-4 space-x-4 text-gray-600">
+                            <a href="https://www.linkedin.com/in/rubendledesma/" target="_blank">
+                                <Image
+                                    src="/assets/images/linkedin.png"
+                                    width={30}
+                                    height={30}
+                                    alt="Picture of the author"
+                                />
+                            </a>
+                            <a href="https://github.com/ElRubenD" target="_blank" >
+                                <Image
+                                    src="/assets/images/github.png"
+                                    width={30}
+                                    height={30}
+                                    alt="Picture of the author"
+                                />
+                            </a>
+                            <a href="mailto:ruben.d.ledesma@gmail.com" target="_blank" >
+                                <Image
+                                    src="/assets/images/sobre.png"
+                                    width={30}
+                                    height={30}
+                                    alt="Picture of the author"
+                                />
+                            </a>
+                        </div>
+                </div>
+            </div>
+        </main>
+    )
+}
---
+++ quicktickets/src/components/UI/ScrollToTopButton.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+const ScrollToTopButton: React.FC = () => {
+    const [isVisible, setIsVisible] = useState<boolean>(false);
+  
+    const toggleVisibility = () => {
+      if (window.scrollY > window.innerHeight / 2) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+  
+    const scrollToTop = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    };
+  
+    useEffect(() => {
+      window.addEventListener('scroll', toggleVisibility);
+      return () => {
+        window.removeEventListener('scroll', toggleVisibility);
+      };
+    }, []);
+  
+    return (
+      <button
+        className={`fixed bottom-4 right-4 p-3 bg-[#FFE047] text-white rounded-full transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}
+        onClick={scrollToTop}
+        style={{ display: isVisible ? 'block' : 'none' }}
+      >
+        <Image
+            src="/assets/images/flecha-arriba.png"
+            width={30}
+            height={30}
+            alt="Picture of the author"
+        />
+      </button>
+    );
+  };
+  
+  export default ScrollToTopButton;
