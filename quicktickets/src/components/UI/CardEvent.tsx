d6fdf0eb4153f466119cf4d5cbaab512e2b91b1e
ElRubenD <rubem.d.ledesma@gmail.com>
2024-10-29 11:23:49 -0300

    CardEvents: arreglo de logica, incorporamos nuevos comparadores que me cambian las caracteristicas / events/index: nuevos apartados, como capacity y location

--- quicktickets/src/components/UI/CardEvent.tsx
+++ quicktickets/src/components/UI/CardEvent.tsx
@@ -8,10 +8,12 @@ interface EventCardProps {
   date: string;
   hour: string;
   price: number;
+  capacity: number;
+  location: string;
   category: string;
 }
 
-const EventCard: React.FC<EventCardProps> = ({ imageUrl, name, date, hour, price, category }) => {
+const EventCard: React.FC<EventCardProps> = ({ imageUrl, name, date, hour, price, capacity, location, category }) => {
   return (
     <div className="flex border rounded-md shadow-md overflow-hidden bg-white mb-8">
       {/* Imagen del evento */}
@@ -26,7 +28,7 @@ const EventCard: React.FC<EventCardProps> = ({ imageUrl, name, date, hour, price
         {/* Botón de favoritos */}
         <button className="absolute top-2 right-2 p-2 bg-white rounded-full shadow-md hover:bg-gray-100 transition">
           <Image
-            src="/assets/images/icons/star.svg" // Ruta de la imagen del icono de favoritos en public
+            src="/assets/images/icons/star.svg"
             width={15}
             height={15}
             alt="Favorito"
@@ -42,18 +44,37 @@ const EventCard: React.FC<EventCardProps> = ({ imageUrl, name, date, hour, price
 
       
       {/* Datos del evento */}
-      <div className="flex flex-col justify-between p-4 w-1/2">
-        <h3 className="font-semibold text-lg">{name}</h3>
-        <p className="text-gray-600 text-xs">{date}</p>
-        <p className="text-gray-600 text-xs mb-4">{hour}</p>
-        <p className="text-green-600 text-xs flex">
-        <Image
-            src="/assets/images/icons/entrada1.svg" // Ruta de la imagen del icono de favoritos en public
-            width={10}
-            height={10}
-            alt="entrada"
-          />
-          {price}</p>
+      <div className="flex flex-col justify-between p-2 w-1/2">
+        <div className="h-1/3 mb-1">
+          <h4 className="font-semibold text-sm">{name}</h4>
+        </div>
+        <div className='flex'>
+          <p className="text-gray-600 text-xs mr-1">{date} |</p>
+          <p className="text-gray-600 text-xs">{location}</p>
+        </div>
+        <div>
+          <p className="text-gray-600 text-xs mb-4">{hour}</p>
+        </div>
+        <div className='flex space-x-2'>
+          <p className="text-green-600 text-xs flex">
+          <Image
+              src="/assets/images/icons/entrada-green.svg" // Ruta de la imagen del icono de favoritos en public
+              width={10}
+              height={10}
+              alt="entrada"
+              className='mr-1'
+            />
+            {price === 0 ? "FREE" : `ARS ${price}`}</p>
+          <p className={`text-xs flex ${capacity === 0 ? 'text-red-600' : 'text-blue-600'}`}>
+          <Image
+              src={`/assets/images/icons/${capacity === 0 ? 'group-red.svg' : 'group-blue.svg'}`}
+              width={10}
+              height={10}
+              alt="Capacidad"
+              className='mr-1'
+            />
+            {capacity === 0 ? "Sold Out" : capacity}</p>
+        </div>
       </div>
     </div>
   );
