b1ec0a9d646a2cdfc06b0918d0ed416154d02e27
ElRubenD <rubem.d.ledesma@gmail.com>
2024-11-25 18:37:22 -0300

    Cambios en Header

--- quicktickets/src/components/UI/Header.tsx
+++ quicktickets/src/components/UI/Header.tsx
@@ -13,6 +13,7 @@ import { MdLogout } from "react-icons/md";
 import { TbCirclePlus } from "react-icons/tb";
 import { CgMenuRound } from "react-icons/cg";
 
+
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [menuOpen, setMenuOpen] = useState<boolean>(false); // Estado para el menú en pantallas pequeñas
@@ -38,23 +39,27 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="bg-[#2D2A3E] flex justify-between items-center h-20 pr-8 ">
+    <nav className="bg-[#2D2A3E] flex justify-between items-center h-20 pr-8">
       {/* Logo */}
-      <div className="flex items-center space-x-2 md:space-x-4">
-        <div className="w-40 h-10 relative">
+      <div className="flex items-center justify-start space-x-0">
+        {/* Ícono */}
+        <div className="w-40 h-12 relative">
           <Image
             src="/assets/images/icons/icon.svg"
             alt="IconoQuickTickets"
             fill
             className="object-contain"
+            sizes="(max-width: 768px) 10vw, 5vw"
           />
         </div>
-        <div className="hidden md:block w-40 h-10 relative">
+        {/* Logo */}
+        <div className="hidden md:block w-40 h-12 relative">
           <Image
             src="/assets/images/icons/quickticketswhite.svg"
             alt="LogoQuickTickets"
             fill
             className="object-contain"
+            sizes="(max-width: 768px) 10vw, 5vw"
           />
         </div>
       </div>
@@ -75,7 +80,7 @@ const NavBar = () => {
         </Link>
       </div>
 
-      {/* Menú para pantallas pequeñas */}
+      {/* Menú desplegable para pantallas pequeñas */}
       <div className="md:hidden flex items-center">
         <button
           onClick={toggleMenu}
@@ -89,7 +94,7 @@ const NavBar = () => {
       </div>
 
       {menuOpen && (
-        <div className="absolute top-20 right-0 bg-[#2D2A3E] w-full md:hidden z-50">
+        <div className="absolute top-20 right-0 bg-[#2D2A3E] w-full md:hidden">
           <Link href="/" className="block text-white hover:text-yellow-400 py-2 px-4">
             Home
           </Link>
@@ -105,63 +110,66 @@ const NavBar = () => {
         </div>
       )}
 
-      {/* Perfil / Login */}
+      {/* Login / Profile */}
       {session ? (
-        <div className="flex items-center gap-6 relative">
-          <Link
-            href="/events/create-event"
-            className="flex flex-col items-center text-white hover:text-yellow-400"
-          >
-            <TbCirclePlus className="text-2xl" />
-            <span className="text-sm hidden sm:block">Create</span>
-          </Link>
-          <Link
-            href="/tickets"
-            className="flex flex-col items-center text-white hover:text-yellow-400"
-          >
-            <IoTicketOutline className="text-2xl" />
-            <span className="text-sm hidden sm:block">Tickets</span>
-          </Link>
-          <div className="relative flex items-center">
-            <button
-              onClick={toggleDropdown}
-              className="flex items-center space-x-1 text-white hover:text-yellow-400"
-            >
-              <div className="flex flex-col items-center">
-                <CgProfile className="text-2xl" />
-                <span className="text-sm hidden sm:block">Profile</span>
+        <div className="flex items-center justify-center gap-6 relative">
+            <div className="flex flex-col items-center justify-center text-sm">
+            <Link href="/events/create-event" className="flex flex-col items-center justify-center text-white hover:text-yellow-400">
+                <TbCirclePlus className="text-2xl" />
+                <span className="text-sm hidden sm:block">Create</span>
+                </Link>
+            </div>
+          <div className="flex gap-6 items-center justify-center relative">
+            <div className="flex flex-col items-center justify-center text-sm">
+              <Link href="/tickets" className="flex flex-col text-white hover:text-yellow-400 items-center">
+                    <IoTicketOutline className="text-white text-2xl" />
+                    <span className="text-sm hidden sm:block">Tickets</span>    
+              </Link>
+            </div>
+            <div className="flex items-center justify-center text-sm gap-2">
+              <div className="flex flex-col items-center justify-center">
+                <Link href="/userProfile" className="flex flex-col text-white hover:text-yellow-400 items-center">
+                    <CgProfile className="text-white text-2xl" />
+                    <span className="text-sm hidden sm:block">Profile</span>
+                </Link>
               </div>
-              <IoMdArrowDropdown className="text-2xl" />
-            </button>
+              <button onClick={toggleDropdown} className="flex items-center justify-center">
+                <IoMdArrowDropdown className="text-white text-2xl " />
+              </button>
+            </div>
             {isOpen && (
-              <div className="absolute top-full right-0 mt-2 bg-white text-black rounded shadow-md w-40 z-50">
+              <div className="absolute bg-white rounded shadow-lg mt-[167px] text-sm min-w-[140px]">
                 <Link
                   href="/userProfile"
-                  className="block px-4 py-2 hover:bg-gray-200"
+                  onClick={() => setIsOpen(false)}
+                  className="block p-2 text-black hover:bg-gray-200 rounded"
                 >
-                  Account Settings
+                  Account setting
                 </Link>
                 <Link
-                  href="/my-events"
-                  className="block px-4 py-2 hover:bg-gray-200 font-semibold text-orange-500"
-                >
-                  My Events
+                 href="/my-events"
+                 onClick={() => setIsOpen(false)}
+                 >
+                  <button
+                  className="block font-bold p-2 text-orange-500 hover:bg-gray-200 rounded">
+                    My Events
+                  </button>
                 </Link>
-                {isAdmin && (
-                  <Link
-                    href="/adminDashboard"
-                    className="block px-4 py-2 hover:bg-gray-200 font-semibold text-violet-500"
+                {isAdmin && (<Link
+                  href="/adminDashboard"
+                  onClick={() => setIsOpen(false)}
+                  className="block font-bold p-2 text-violet-500 hover:bg-gray-200 rounded"
+                >Admin Dashboard
+                </Link>)}
+                <div className="flex justify-between hover:bg-gray-200 rounded">
+                  <button
+                    onClick={handleLogout}
+                    className="flex items-center p-2 gap-2 w-full text-start text-red-500 font-bold"
                   >
-                    Admin Dashboard
-                  </Link>
-                )}
-                <button
-                  onClick={handleLogout}
-                  className="flex items-center gap-2 px-4 py-2 text-red-500 font-semibold hover:bg-gray-200 w-full"
-                >
-                  <MdLogout className="text-lg" />
-                  <span>Logout</span>
-                </button>
+                    Logout
+                    <MdLogout />
+                  </button>
+                </div>
               </div>
             )}
           </div>
@@ -171,10 +179,7 @@ const NavBar = () => {
           <Link href="/login" className="text-white hover:text-yellow-400">
             Login
           </Link>
-          <Link
-            href="/register"
-            className="bg-yellow-400 text-black font-bold py-2 px-4 rounded hover:bg-yellow-300"
-          >
+          <Link href="/register" className="bg-yellow-400 text-black font-bold py-2 px-4 rounded hover:bg-yellow-300">
             Sign Up
           </Link>
         </div>
